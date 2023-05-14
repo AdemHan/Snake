8d4a11e5dc90f2fb0ba3351508b22980d9060f2a
Adem Han <adem.han@outlook.com>
2023-05-14 05:36:02 +0300

    Options menu added

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;  //ses dosyasý referansý için gerekli
+
+    public Dropdown resolutionDropdown;  // referans vermemiz için gerekli bir deðiþken
+
+    Resolution[] resolutions;  //resolution yani ölçeklendirme verilerini tutan bir dizi
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;  //ekran boyutunu resolution degiskenine atadik
+
+        resolutionDropdown.ClearOptions();  // resolutionDropdown referansýndaki tüm secenekleri sildik
+
+        List<string> options = new List<string>();  //seceneklerimizi listeye ekleyip her boyut için yazi ekleyeceðiz. sonra onlarý resolutionDropdowna aktaricaz. O yüzden bir list olusturduk.
+
+        for (int i = 0; i < resolutions.Length; i++)  //dizinin boyutunda bir dizi olusturduk
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;  // i indexli eleman için en boy verilerini alýp seçenek adýndaki degiskene atadik.
+            options.Add(option);   // seçenekler listesine, seçenek adýndaki degiskeni atadik
+        }
+
+        resolutionDropdown.AddOptions(options);     // resolutionDropdown degiskenine options listesini ekledik
+    }
+
     public void PlayGame()
     {
         //Burada aktif olan sahnemizin index numarasýna bir ekledik ve yüklemesini soyledik. Yani build settingsdeki siradaki sahneyi yüklemesini soyledik.
@@ -15,6 +40,21 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("QUIT!");
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)    //Ses seviyesini audioMixer aracýlýðýyla slidera baðlamýþtýk. Deðerin atamasýný burada yaptýk.
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality (int qualityIndex)  //Grafik kalitesini Quality ayarlarýndaki index numarasina eriþerek düzenledik.
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)  // ekranýn tam ekran oluð olmama durumunu bool cinsinden kontrol ediyoruz.
+    {
+        Screen.fullScreen = isFullscreen;
     }
 
 }
